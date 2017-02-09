936affbe5ff847382f343165534fab16170bfbf1
Ben Zadeh <benzadeh@gmail.com>
2017-02-08 19:07:48 -0500

    remove and add

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace AzureCDN
+{
+    class Program
+    {
+        static void Main()
+        {
+            var accountName = "cloudprograming";
+            var accountKey = "8dsawYJgjl6/6P4XCN3q9EUWmwPupEtrjHBTZ3pFxe7EPThkgeaV7MDYVpARcHin77OVUPWr8G7Z/IJkAeeTKQ==";
+
+            //set CacheControl to one hour expiration
+            var cacheControl = "public, max-age=50";
+
+            try
+            {
+                Console.WriteLine($"Connecting to Azure Storage account {accountName} and creating client.");
+                var account = CloudStorageAccount.Parse($"DefaultEndpointsProtocol=https;AccountName={accountName};AccountKey={accountKey}");
+                var client = account.CreateCloudBlobClient();
+
+                Console.WriteLine($"Iterating through all containers in account {accountName}");
+                foreach (var container in client.ListContainers(null, ContainerListingDetails.All))
+                {
+                    Console.WriteLine($"Now inspecting container {container.Name}");
+                    var permissions = container.GetPermissions();
+                    if (permissions.PublicAccess == BlobContainerPublicAccessType.Blob || permissions.PublicAccess == BlobContainerPublicAccessType.Container)
+                    {
+                        Console.WriteLine($"Container {container.Name} is a public container or has public blobs, proceeding.");
+                        //we want to get a flat listing of blobs, since we're going to iterate them all
+                        foreach (var blobItem in container.ListBlobs(null, true))
+                        {
+                            //we only want to cache-control block blobs
+                            if (blobItem is CloudBlockBlob)
+                            {
+                                //cast IListBlobItem to CloudBlockBlob
+                                var blob = (CloudBlockBlob)blobItem;
+                                if (blob.Exists())
+                                {
+                                    Console.WriteLine($"Blob {blob.Name} is a CloudBlockBlob and it exists. Setting CacheControl to {cacheControl}");
+                                    blob.Properties.CacheControl = cacheControl;
+                                    blob.SetProperties();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error encountered: {ex.Message}");
+            }
+
+            Console.Read();
+        }
+    }
+}
